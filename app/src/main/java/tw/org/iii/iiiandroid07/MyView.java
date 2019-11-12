166b45c36266b68e0be45d7316b0f7050f905145
brad <brad@brad.tw>
2019-11-12 16:48:27 +0800

    4

--- app/src/main/java/tw/org/iii/iiiandroid07/MyView.java
+++ app/src/main/java/tw/org/iii/iiiandroid07/MyView.java
@@ -1,7 +1,9 @@
 package tw.org.iii.iiiandroid07;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -12,5 +14,14 @@ public class MyView extends View {
     public MyView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         setBackgroundColor(Color.BLUE);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        Paint paint = new Paint();
+        paint.setColor(Color.YELLOW);
+        paint.setStrokeWidth(10);
+        canvas.drawLine(0, 0, 200, 200, paint);
     }
 }
